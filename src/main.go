71eddcd65eba6e66946a3f6341c3c3428eeb6dbc
Kevin Chen <ykevinc@gmail.com>
2013-01-23 12:44:18 -0800

    First run

---
+++ src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kevinchen
+ * Date: 1/23/13
+ * Time: 12:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello world!")
+}
