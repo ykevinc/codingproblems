52f2fab8366008fd7ea04f4503ea6a49d6a8b9c5
ykevinc <ykevinc@gmail.com>
2016-02-03 12:54:02 -0800

    Median of two sorted sorry

---
+++ src/uva/Leetcode00004.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Leetcode00004 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int merged[] = new int[nums1.length + nums2.length];
+        int l = 0;
+        int r = 0;
+        int i = 0;
+        while (l < nums1.length || r < nums2.length ) {
+            if (l < nums1.length && r < nums2.length) {
+                if (nums1[l] < nums2[r]) {
+                    merged[i++] = nums1[l++];
+                } else {
+                    merged[i++] = nums2[r++];
+                }
+            } else if (l < nums1.length) {
+                merged[i++] = nums1[l++];
+            } else {
+                merged[i++] = nums2[r++];
+            }
+        }
+        //System.out.println(Arrays.toString(merged));
+        if (merged.length % 2 == 0) {
+            return (merged[merged.length/2-1] + merged[merged.length/2])/2.0;
+        } else {
+            return merged[merged.length/2];
+        }
+    }
+    
+}
