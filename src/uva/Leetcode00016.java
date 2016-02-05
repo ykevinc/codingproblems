cd9ce94a666b18e6da9539c0d977166c11335ade
ykevinc <ykevinc@gmail.com>
2016-02-05 07:10:24 -0800

    3Sum to target

---
+++ src/uva/Leetcode00016.java
@@ -0,0 +1,30 @@
+public class Leetcode00016 {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        int result = 0;
+        for (int i = 0;i < nums.length;i++) {
+    		int j = i + 1;
+    		int k = nums.length - 1;
+    		while (j < k) {
+    			int sum = nums[i] + nums[j] + nums[k];
+    			int diff = Math.abs(sum - target);
+     
+    			if(diff == 0) {
+    			    return sum;
+    			}
+     
+    			if (diff < min) {
+    				min = diff;
+    				result = sum;
+    			}
+    			if (sum <= target) {
+    				j++;
+    			} else {
+    				k--;
+    			}
+    		}
+        }
+        return result;
+    }
+}
