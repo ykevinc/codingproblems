e995f9fb8bd5dcf7e0838beb20eb2eb8cc79241d
ykevinc <ykevinc@gmail.com>
2016-05-11 13:40:21 -0700

    Remove extra statement

--- src/uva/Leetcode00002.swift
+++ src/uva/Leetcode00002.swift
@@ -14,7 +14,6 @@ class Solution {
         var l : ListNode? = l1, r : ListNode? = l2, c = 0
         let head = ListNode(0)
         var node : ListNode = head;
-        var vv = l != nil ? 1 : 0
         while l != nil || r != nil || c > 0 {
             var s : Int = (l?.val ?? 0) + (r?.val ?? 0) + c
             c = s / 10
