a5b881bdbd97e91abf01971c9b1ceeea1183bb08
ykevinc <ykevinc@gmail.com>
2016-05-13 10:19:33 -0700

    Cleaner logic

--- src/uva/Leetcode00006.swift
+++ src/uva/Leetcode00006.swift
@@ -1,25 +1,31 @@
 class Solution {
     func convert(s: String, _ numRows: Int) -> String {
-        var letters = s.characters.map{Character(String($0))}
-        var arr = Array(count: numRows, repeatedValue: Array(count: s.characters.count, repeatedValue: Character("\0")))
-        var x = 0, y = 0
-        var dirDown = true
-        for c in letters {
+        var arr = Array(count: numRows, repeatedValue: Array(count: min(s.characters.count, 500), repeatedValue: Character("\0")))
+        var x = 0, y = 0, dir = 0
+        var dirs : [[Int]] = [[1,0],[-1,1],[0,1]]
+        var maxy = 0
+        for c in s.characters {
             arr[x][y] = c
-            if dirDown {
-                (x, y, dirDown) = (x + 1) % numRows == 0 ? (max(x - 1, 0), y + 1, false) : (min(x + 1,numRows-1), y, true)
-            } else {
-                (x, y, dirDown) = x == 0 ? (min(x + 1,numRows-1), y, true) : (max(x - 1, 0), y + 1, false)
+            if numRows == 1 {
+                dir = 2
+            } else if x == numRows - 1 {
+                dir = 1
+            } else if x == 0 {
+                dir = 0
             }
+            x += dirs[dir][0]
+            y += dirs[dir][1]
+            maxy = max(maxy, y)
         }
         var sb = ""
+        var array = [String]()
         for x in 0..<numRows {
-            for y in 0..<s.characters.count {
+            for y in 0..<maxy {
                 if arr[x][y] != "\0" {
-                    sb += String(arr[x][y])
+                    array.append(String(arr[x][y]))
                 } 
             }
         }
-        return sb
+        return array.joinWithSeparator("")
     }
 }
