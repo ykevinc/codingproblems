092f53c0f46121af4e5313bdc67af467685462b1
ykevinc <ykevinc@gmail.com>
2018-01-14 11:11:26 -0800

    Maximum substring with no repeat

---
+++ src/uva/Leetcode00003.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	i, j := 0, 0
+	m := 0
+	w := make(map[byte]bool, len(s))
+	for i < len(s) && j < len(s) {
+		c := s[j]
+		if _, ok := w[c]; !ok {
+			w[c] = true
+			if len(w) > m {
+				m = len(w)
+			}
+			j++
+		} else {
+			delete(w, s[i])
+			i++
+		}
+	}
+	return m
+}
