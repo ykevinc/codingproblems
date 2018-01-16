ddc3444158a65ddac9f7d200745cf2fadeab70ed
ykevinc <ykevinc@gmail.com>
2018-01-15 21:31:07 -0800

    Largest number optimized

--- src/main/Leetcode00179.go
+++ src/main/Leetcode00179.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"strings"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 func largestNumber(nums []int) string {
@@ -10,46 +11,16 @@ func largestNumber(nums []int) string {
 	for _, n := range nums {
 		strs = append(strs, strconv.Itoa(n))
 	}
-	m := make([][]string, 10, 10)
-	for _, s := range strs {
-		m[s[0]-'0'] = append(m[s[0]-'0'], s)
-	}
-	if len(m[0]) == len(nums) {
+
+	sort.Slice(strs, func(i, j int) bool {
+		s1 := strs[i] + strs[j]
+		s2 := strs[j] + strs[i]
+		return s1 > s2
+	})
+
+	if strs[0] == "0" {
 		return "0"
 	}
-	max := ""
-	dfs(m, 9, "", &max)
-	return max
-}
 
-func dfs(m [][]string, d int, c string, max *string) {
-	if d == -1 {
-		return
-	}
-	if len(c) > len(*max) {
-		*max = c
-	} else if len(c) == len(*max) {
-		for i := 0; i < len(c); i++ {
-			if c[i] > (*max)[i] {
-				*max = c
-				break
-			} else if c[i] < (*max)[i] {
-				break
-			}
-		}
-	}
-	v := m[d]
-	choices := 0
-	for i, s := range v {
-		if s == "@" {
-			continue
-		}
-		choices++
-		v[i] = "@"
-		dfs(m, d, c+s, max)
-		v[i] = s
-	}
-	if choices == 0 {
-		dfs(m, d-1, c, max)	
-	}
+	return strings.Join(strs, "")
 }
